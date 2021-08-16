a0c9b3e6093d8971f02242e2eaf9b6e2a2066312
mich <6277211+m1ch@users.noreply.github.com>
2021-08-16 15:38:36 +0200

    update

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # iocage-plugin-mariadb
+
+## create the jail
+iocage fetch -P ~/iocage-plugin-index/mariadb-kiss.json --name "test_mariadb" ip4_addr="vnet0|10.23.10.61/24" interfaces="vnet0:bridge10" vnet=1 defaultrouter="10.23.10.1"
+
+iocage fetch --plugin-name "mariadb-kiss" --git_repository https://github.com/m1ch/iocage-plugin-index --branch kiss --name "test_redis" interfaces="vnet0:bridge10" vnet=1 defaultrouter="10.23.10.1" resolver="search local;nameserver 1.1.1.1"
--- post_install.sh
+++ post_install.sh
@@ -1,7 +1,7 @@
 #!/usr/local/bin/bash
 sysrc mysql_enable="YES"
 sysrc mysql_pidfile=/var/db/mysql/mysql.pid
-sysrc mysql_optfile=/usr/local/etc/my.cnf
+# sysrc mysql_optfile=/usr/local/etc/my.cnf
 
 mkdir /var/log/mysql
 chown mysql /var/log/mysql
@@ -9,26 +9,31 @@ chown mysql /var/log/mysql
 echo "Start mariaDB server"
 service mysql-server start
 
-USER="root"
-openssl rand -base64 23  > /root/mysqlrootpassword
+MYUSER="root"
+openssl rand -base64 23 | sed -e 's/=//g' > /root/mysqlrootpassword
 PASS=$(</root/mysqlrootpassword)
 
 echo "Passwort = $PASS"
 
 # set mysql-password
-mysqladmin --user=root password "$PASS"
+mysqladmin --user=$MYUSER password "$PASS"
 echo "MySQL passwort set"
 
 # Configure mysql
-mysql -u root -p"${PASS}" << EOF
+mysql -u $MYUSER -p"${PASS}" << EOF
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${PASS}';
-GRANT ALL PRIVILEGES ON *.* TO '${USER}'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO '${MYUSER}'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO '${MYUSER}'@'127.0.0.1' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 DROP DATABASE IF EXISTS test;
 EOF
 
 # chmod +x /usr/local/bin/adminer.sh
-echo "@reboot /usr/locat/bin/adminer.sh" | crontab -
+echo "@reboot /usr/local/bin/adminer.sh" | crontab -
+
+at now + 1minute << EOL
+/usr/local/bin/adminer.sh
+EOL
 
 # /usr/local/www/adminer # php -S ip:8080
 # php -S `ifconfig | grep "inet " | grep -v 127.0.0.1 | cut -d" " -f2`:8080
